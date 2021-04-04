e554eaf60339f5f4aec1bdee623df701b5882ab6
Takumi Miyoshi <34400mt@gmail.com>
2021-04-04 22:42:44 +0900

    add step function to the KdV class

--- KortewegDeVries.cpp
+++ KortewegDeVries.cpp
@@ -1,32 +1,50 @@
 #include "KortewegDeVries.h"
 
-KortewegDeVries::KortewegDeVries(int numberOfSpaceCoordinatePoints, std::function<double(double, double)>& initialWave, double timeInterval, double spaceInterval)
+KortewegDeVries::KortewegDeVries(int numberOfSpaceCoordinatePoints, const std::function<double(double, double)>& initialWave, double timeInterval, double spaceInterval)
 	:m_numberOfSpaceCoordinatePoints{ numberOfSpaceCoordinatePoints }, m_timeInterval{ timeInterval }, m_spaceInterval{ spaceInterval }
 {
 	// allocate the memory
-	m_eta.reserve(m_numberOfSpaceCoordinatePoints);
+	m_height.reserve(m_numberOfSpaceCoordinatePoints);
 	m_velocity.reserve(m_numberOfSpaceCoordinatePoints);
-	m_acceleration.reserve(m_numberOfSpaceCoordinatePoints);
 	
 	// initialize wave
 	initializeWave(initialWave);
 }
 
-void KortewegDeVries::initializeWave(std::function<double(double, double)>& initialWave)
+void KortewegDeVries::initializeWave(const std::function<double(double, double)>& initialWave)
 {
 	double spaceCoordinate{ 0.0 };
-	for (int count{ 0 };count<m_numberOfSpaceCoordinatePoints;++count)
+	for (int point{ 0 };point<m_numberOfSpaceCoordinatePoints;++point)
 	{
 		// set the initial wave height
-		m_eta[count] = initialWave(0, spaceCoordinate);
+		m_height[point] = initialWave(0, spaceCoordinate);
 		// set the initial wave velocity using central difference
-		m_velocity[count] = (initialWave(m_timeInterval, spaceCoordinate) - initialWave(-m_timeInterval, spaceCoordinate)) * 0.5 / m_timeInterval;
-		// set the initial wave acceleration using central difference
-		m_acceleration[count] = (initialWave(2 * m_timeInterval, spaceCoordinate) + initialWave(-2 * m_timeInterval, spaceCoordinate) - 2 * m_eta[count]) 
-								* 0.25 / (m_timeInterval * m_timeInterval);
-
+		m_velocity[point] = (initialWave(m_timeInterval, spaceCoordinate) - initialWave(-m_timeInterval, spaceCoordinate)) * 0.5 / m_timeInterval;
+		
 		spaceCoordinate += m_spaceInterval;
 	}
 }
 
-const std::vector<double>& KortewegDeVries::getWaveHeight() const { return m_eta; }
+void KortewegDeVries::stepForward(int steps)
+{
+	double nextEta{};
+	double nextVelocity{};
+
+	for (int step{ 0 }; step < steps; ++step)
+	{
+		for (int point{ 0 }; point < m_numberOfSpaceCoordinatePoints; ++point)
+		{
+			nextEta = m_height[point] + m_velocity[point] * m_timeInterval;
+			nextVelocity = m_height[point] * getFirstOrderSpatialCentralDifference(point) + getThirdOrderSpatialCentralDifference(point);
+		}
+	}
+
+}
+
+double KortewegDeVries::getFirstOrderSpatialCentralDifference(int point) const { return (m_height[next(point)] - m_height[prev(point)]) * 0.5 / m_spaceInterval; }
+double KortewegDeVries::getThirdOrderSpatialCentralDifference(int point) const
+{
+	return (m_height[next(point, 3)] - 3 * m_height[next(point)] + 3 * m_height[prev(point)] + m_height[prev(point, 3)]) * 0.125 / (m_spaceInterval * m_spaceInterval * m_spaceInterval);
+}
+
+const std::vector<double>& KortewegDeVries::getWaveHeight() const { return m_height; }
--- KortewegDeVries.h
+++ KortewegDeVries.h
@@ -7,11 +7,9 @@ class KortewegDeVries
 private:
 	int m_numberOfSpaceCoordinatePoints{};
 	// height of the water surface
-	std::vector<double> m_eta;
+	std::vector<double> m_height;
 	// velocity of the water surface
 	std::vector<double> m_velocity;
-	// acceleration of the water surface
-	std::vector<double> m_acceleration;
 
 	// constant
 	// time interval
@@ -19,11 +17,28 @@ private:
 	// space interval
 	double m_spaceInterval{};
 
-	void initializeWave(std::function<double(double, double)>& initialWave);
+	void initializeWave(const std::function<double(double, double)>& initialWave);
+	double getFirstOrderSpatialCentralDifference(int point) const;
+	double getThirdOrderSpatialCentralDifference(int point) const;
+
+	inline int prev(int point, int n = 1) const
+	{
+		return (point - n + m_numberOfSpaceCoordinatePoints) % m_numberOfSpaceCoordinatePoints;
+	}
+
+	inline int next(int point, int n = 1) const
+	{
+		return (point + n) % m_numberOfSpaceCoordinatePoints;
+	}
+
+	// soliton solution of KdV equation
+	static double solitonSolution(double time, double space);
 
 public:
 	// initialWave returns the wave height with the argument as (time coordinate, space coordinate) 
-	KortewegDeVries(int numberOfSpaceCoordinatePoints, std::function<double(double, double)>& initialWave, double timeInterval = 0.1, double spaceInterval = 0.1);
+	KortewegDeVries(int numberOfSpaceCoordinatePoints, const std::function<double(double, double)>& initialWave = solitonSolution, double timeInterval = 0.1, double spaceInterval = 0.1);
 	
+	void stepForward(int steps = 1);
+
 	const std::vector<double>& getWaveHeight() const;
 };
